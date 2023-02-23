cc84b045eb4eb22148042ca3aef950d9fc5f4f32
Deividas <deividas.aldonis.dev@gmail.com>
2023-02-23 02:11:18 +0200

    fetch data from ipify api and display it

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+const submitBtn = document.querySelector(".submit-btn");
+const formInput = document.querySelector(".form-input");
 const ipifyURL = "https://ipapi.co/5.20.185.228/json/";
 const geoIpifyURL =
   "https://geo.ipify.org/api/v2/country?apiKey=at_WaeVZbFRO3gB3eI5kPvLg0xbYDD3y&ipAddress=";
+
+const ipEl = document.querySelector(".data-ip");
+const ispEl = document.querySelector(".data-isp");
+const locationEl = document.querySelector(".data-location");
+const timezoneEl = document.querySelector(".data-timezone");
 
 const checkBalance = async () => {
   //   const res = await fetch(ipifyURL);
@@ -34,3 +41,51 @@ const checkBalance = async () => {
 };
 
 checkBalance();
+
+const getCoords = async (userInput) => {
+  try {
+    const res = await fetch(`https://ipapi.co/${userInput}/json/`);
+    const data = await res.json();
+
+    if (data.error) {
+      alert(`An error occurred\nReason: ${data.reason} (${data.ip})`);
+      return;
+    }
+
+    const {
+      ip,
+      city,
+      country,
+      postal,
+      isp,
+      utc_offset,
+      org,
+      latitude,
+      longitude,
+    } = data;
+    const utc = utc_offset.slice(0, 3) + ":" + utc_offset.slice(3);
+
+    ipEl.textContent = ip;
+    ispEl.textContent = org;
+    locationEl.textContent = `${city}, ${country} ${postal}`;
+    timezoneEl.textContent = `UTC ${utc}`;
+
+    ipEl.classList.remove("hidden");
+    ispEl.classList.remove("hidden");
+    locationEl.classList.remove("hidden");
+    timezoneEl.classList.remove("hidden");
+  } catch (error) {
+    console.error(error);
+    alert("An error occurred, please try again later.");
+  }
+};
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const inputValue = formInput.value.trim();
+
+  if (inputValue.length < 1) return;
+
+  getCoords(inputValue);
+});
